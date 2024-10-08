ea7d8ada149265805e21900d881eae545313c802
Github Sync Pipeline <axytos-ecommerce-plugins@brickmakers.de>
2024-10-08 08:46:31 +0000

    Publish Version 1.7.0-rc, Build 20241008.5

--- src/Model/Actions/ActionExecutorInterface.php
+++ src/Model/Actions/ActionExecutorInterface.php
@@ -5,10 +5,11 @@ namespace Axytos\KaufAufRechnung\Core\Abstractions\Model\Actions;
 interface ActionExecutorInterface
 {
     /**
-     * @param string $clientSecret
-     * @param string $action
+     * @param string                   $clientSecret
+     * @param string                   $action
      * @param array<string,mixed>|null $parameters
-     * @return \Axytos\KaufAufRechnung\Core\Abstractions\Model\Actions\ActionResultInterface
+     *
+     * @return ActionResultInterface
      */
     public function executeAction($clientSecret, $action, $parameters = null);
 }
